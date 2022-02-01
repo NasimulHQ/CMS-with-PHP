2dfea64d4a5e1e8cbc023f82bf4b238d9fb75a58
NasimulHQ <nasimul345@gmail.com>
2022-02-01 23:48:34 +0600

    feat: Refactoring category

--- admin/categories.php
+++ admin/categories.php
@@ -18,25 +18,8 @@
 
                      <div class="col-xs-6">
 
-                         <?php //Create category
-                            if (isset($_POST['submit'])) {
-                                $cat_title = $_POST['cat_title'];
+                         <?php insert_categories(); ?>
 
-                                // form validation filed should not be empty
-                                if ($cat_title == "" || empty($cat_title)) {
-                                    echo "This Field should not be empty";
-                                } else {
-
-                                    $query = "INSERT INTO categories(cat_title) ";
-                                    $query .= "VALUE('{$cat_title}') ";
-                                    $create_category_query = mysqli_query($connection, $query);
-                                    if (!$create_category_query) {
-                                        die('QUERY FAILED') . mysqli_error($connection);
-                                    }
-                                }
-                            }
-
-                            ?>
                          <form action="" method="post">
                              <div class="form-group">
                                  <label for="cat-title">Category Name</label>
@@ -47,17 +30,15 @@
                              </div>
                          </form>
 
+                       <?php
 
-                      <?php 
-                      
-                      if(isset($_GET['edit'])){
-                          $cat_id = $_GET['edit'];
+                            if (isset($_GET['edit'])) {
+                                $cat_id = $_GET['edit'];
 
-                          include "includes/update_categories.php";
-                      }
-                      
-                      ?>
+                                include "includes/update_categories.php";
+                            }
 
+                            ?>
 
                      </div> <!-- category form -->
                      <div class="col-xs-6">
@@ -70,30 +51,9 @@
 
                              </thead>
                              <tbody>
-                                 <?php  // Find All Category
-                                    $query = "SELECT * FROM categories";
-                                    $select_categories = mysqli_query($connection, $query);
-                                    while ($row = mysqli_fetch_assoc($select_categories)) {
-                                        $cat_id = $row['cat_id'];
-                                        $cat_title = $row['cat_title'];
-                                        echo "<tr>";
-                                        echo "<td>{$cat_id}</td>";
-                                        echo "<td>{$cat_title}</td>";
-                                        echo "<td><a href='categories.php?delete={$cat_id}'>Delete</a></td>";
-                                        echo "<td><a href='categories.php?edit={$cat_id}'>Edit</a></td>";
-                                        echo "</tr>";
-                                    }
-                                    ?>
 
-                                 <?php // Category Delete
-                                    if (isset($_GET['delete'])) {
-                                        $the_cat_id = $_GET['delete'];
-                                        $query = "DELETE FROM categories WHERE cat_id = {$the_cat_id}";
-                                        $delete_query = mysqli_query($connection, $query);
-                                        header("Location: categories.php");
-                                    }
-
-                                    ?>
+                                 <?php findAllCategories(); ?>
+                                 <?php deleteCategories(); ?>
 
                              </tbody>
                          </table>
