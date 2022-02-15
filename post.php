1f76a6f109deea713bef964db88b8dffc1ef576b
NasimulHQ <nasimul345@gmail.com>
2022-02-16 00:10:29 +0600

    feat: comment insert to database in admin panel

--- post.php
+++ post.php
@@ -15,8 +15,8 @@
         <div class="col-md-8">
             <!-- post data bringing up here -->
             <?php
-            if(isset($_GET['p_id'])){
-               $the_post_id = $_GET['p_id'];
+            if (isset($_GET['p_id'])) {
+                $the_post_id = $_GET['p_id'];
             }
 
             $query = "SELECT * FROM posts WHERE post_id = $the_post_id ";
@@ -55,13 +55,30 @@
 
             <!-- Blog Comments -->
 
-            <?php 
+            <?php
+
+            if (isset($_POST['create_comment'])) {
+
+                $the_post_id = $_GET['p_id'];
+                $comment_author = $_POST['comment_author'];
+                $comment_email = $_POST['comment_email'];
+                $comment_content = $_POST['comment_content'];
+
+         $query = "INSERT INTO comments (comment_post_id, comment_author, comment_email, comment_content, comment_status,comment_date)";
+
+        $query .= "VALUES ($the_post_id ,'{$comment_author}', '{$comment_email}', '{$comment_content}', 'unapproved',now())";
+
+         $create_comment_query = mysqli_query($connection, $query);
+
+                    if (!$create_comment_query) {
+                        die('QUERY FAILED' . mysqli_error($connection));
+                    }
+                }
             
-            if(isset($_POST['create_comment'])){
-             echo   $_POST['comment_author'];
-            }
-            
-            
+
+
+
+
             ?>
 
             <!-- Comments Form -->
