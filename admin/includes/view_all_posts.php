436cbdda572bcc5d8be80c92e77a18690c4a06fa
NasimulHQ <nasimul345@gmail.com>
2022-02-20 18:58:49 +0600

    feat: post edit & delete header

--- admin/includes/view_all_posts.php
+++ admin/includes/view_all_posts.php
@@ -11,6 +11,8 @@
             <th>Content</th>
             <th>Comments</th>
             <th>Date</th>
+            <th>Edit</th>
+            <th>Delete</th>
         </tr>
     </thead>
     <tbody>
