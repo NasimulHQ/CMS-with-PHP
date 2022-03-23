dc79576f1182f86d5e66692d8be6bad97e89067d
NasimulHQ <nasimul345@gmail.com>
2022-03-23 18:07:07 +0600

    feat: login create in sidebar/ html

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -20,32 +20,51 @@
          <!-- /.input-group -->
      </div>
 
-     <!-- Blog Categories Well -->
+     <!-- Login Well -->
      <div class="well">
-         <h4>Blog Categories</h4>
-         <?php
-
-            $query = "SELECT * FROM categories";
-            $select_categories_sidebar = mysqli_query($connection, $query);
-
-            ?>
-         <div class="row">
-             <div class="col-lg-12">
-                 <ul class="list-unstyled">
-                     <?php
-                        while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
-                            $cat_title = $row['cat_title'];
-                            $cat_id = $row['cat_id'];
-                            echo "<li><a href='category.php?category_id= $cat_id'>{$cat_title}</li>";
-                        }
-                        ?>
-                 </ul>
+         <h4>Login</h4>
+         <form action="includes/login.php" method="post">
+             <div class="form-group">
+                 <input name="username" type="text" class="form-control" placeholder="Enter the username">
              </div>
-            
-         </div>
-         <!-- /.row -->
+             <div class="form-group">
+                 <input name="password" type="password" class="form-control" placeholder="Enter the password">
+
+                 <span>
+                     <button class="btn btn-primary" name="login" type="submit" style="margin-top: 10px;">Submit</button>
+                 </span>
+             </div>
+         </form>
+         <!--search form-->
+         <!-- /.input-group -->
      </div>
 
-     <?php include "widget.php" ?>
+     <!-- Blog Categories Well -->
+     <div class=" well">
+                         <h4>Blog Categories</h4>
+                         <?php
 
- </div>
+                            $query = "SELECT * FROM categories";
+                            $select_categories_sidebar = mysqli_query($connection, $query);
+
+                            ?>
+                         <div class="row">
+                             <div class="col-lg-12">
+                                 <ul class="list-unstyled">
+                                     <?php
+                                        while ($row = mysqli_fetch_assoc($select_categories_sidebar)) {
+                                            $cat_title = $row['cat_title'];
+                                            $cat_id = $row['cat_id'];
+                                            echo "<li><a href='category.php?category_id= $cat_id'>{$cat_title}</li>";
+                                        }
+                                        ?>
+                                 </ul>
+                             </div>
+
+                         </div>
+                         <!-- /.row -->
+             </div>
+
+             <?php include "widget.php" ?>
+
+     </div>
